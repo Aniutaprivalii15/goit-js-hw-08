04179ec833e0bbf2b50df9a5edf38d0d06c0ecec
Anna <anytka141528@gmail.com>
2023-01-09 03:39:17 +0200

    hm-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,11 +1,12 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-import SimpleLightbox from "simplelightbox";
 // Change code below this line
 
-console.log(SimpleLightbox); 
+console.log(galleryItems);
 
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
 const items = [];
